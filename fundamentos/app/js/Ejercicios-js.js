cce8d203c352da1f6a08187eede70b2616a4d64b
algarrido <97lidialgm@gmail.com>
2021-09-29 10:36:09 +0200

    Pruebas unitarias (mejorar)

--- fundamentos/app/js/Ejercicios-js.js
+++ fundamentos/app/js/Ejercicios-js.js
@@ -23,22 +23,28 @@ function adivinaNum(min, max) {
     alert("SEGUNDO EJERCICIO -- Numero generado: " + numEntero);
     while (intentos <= 10){ 
         respuesta = prompt("Introduce un numero para adivinar" + " llevas " +intentos + " intentos");
+        if(respuesta >= 1 || respuesta <= 100){
         if(numEntero==respuesta){
-                alert("Acertaste!"); 
-                intentos=11;
-        }   
-        else{
-            if(respuesta > numEntero){
-                   
-                alert("El numero a acertar es menor");
-                intentos++;
+                        alert("Acertaste!"); 
+                        intentos=11;
+                }   
+                else{
+                    if(respuesta > numEntero){
+                        
+                        alert("El numero a acertar es menor");
+                        intentos++;
+                    }
+                    else{
+                        
+                        alert("el numero a acertar es mayor"); 
+                        intentos++;    
+                    }
+                }
+            }else{
+                 alert("Introduzca un valor valido");
+                 intentos++; 
             }
-            else{
-                
-                alert("el numero a acertar es mayor"); 
-                intentos++;    
-            }
-        }
+        
     }  
 
 }
@@ -78,8 +84,51 @@ function numerosPrimos(){
     alert(primos);
       
 }
+/**
+ * Crear una función que valide un NIF
+ */
+function validaDNI(dni){
+    let numero, letr, letra;
+    let expresion_regular_dni = /^[XYZ]?\d{5,8}[A-Z]$/;
+    
+    dni = dni.toUpperCase();
+    
+    if(expresion_regular_dni.test(dni) === true){
+        numero = dni.substr(0,dni.length-1);
+        numero = numero.replace('X', 0);
+        numero = numero.replace('Y', 1);
+        numero = numero.replace('Z', 2);
+        letr = dni.substr(dni.length-1, 1);
+        numero = numero % 23;
+        letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
+        letra = letra.substring(numero, numero+1);
+        if (letra != letr) {
+            //alert('Dni erroneo, la letra del NIF no se corresponde');
+            return false;
+        }else{
+            //alert('Dni correcto');
+            return true;
+        }
+    }else{
+            //alert('Dni erroneo, formato no válido');
+            return false;
+        }
+}
 
-function validaDNI(){
+/**
+ * Definir una función que determine si la cadena de texto que se
+ * le pasa como parámetro es un palíndromo, es decir, si se lee de 
+ * la misma forma desde la izquierda y desde la derecha. 
+ * Ejemplo de palíndromo complejo: "La ruta nos aporto otro paso natural".
+ */
 
+function palindromo(str) {
+    const strReversed = str.split("").reverse().join("")
+  
+    if (strReversed === str)  {
+        return true;
+    }else{
+        return false;
+    }    
 }
 
--- fundamentos/spec/Ejercicios.spec.js
+++ fundamentos/spec/Ejercicios.spec.js
@@ -4,10 +4,84 @@ fdescribe('Ejercicios', () =>{
         it('Es entero', ()=> {
 
             let enteroPositivo = numAleatorio();
-
-            expect(enteroPositivo).toBeTruthy;
+            expect(enteroPositivo).toBeTruthy();
 
         })
 
+    })
+
+    describe('Ejercicio 2', () =>{
+
+        it('Es entero positivo', ()=> {
+
+            let enteroPositivo = 50;
+            expect(enteroPositivo).toBeTruthy();
+
+        })
+
+        it('Es negativo', ()=> {
+
+            let negativo = -50;
+            expect(negativo).toBeFalsy;
+
+        })
+
+        it('Es letra', ()=> {
+
+            let letra = '-50';
+            expect(letra).toBeFalse;
+
+        })
+
+        it('Es indefinido', ()=> {
+
+            let ind = undefined;
+            expect(ind).toBeFalsy;
+
+        })
+
+
+    })
+
+    describe('Ejercicio 4', () =>{
+        
+        it('DNI valido', ()=> {
+
+            let dniValido = validaDNI("20621513N");
+            expect(dniValido).toBeTrue();
+
+        })
+
+        it('DNI invalido', ()=> {
+
+            let dniValido = validaDNI("206211513N");
+            expect(dniValido).toBeFalse();
+
+        })
+
+        it('DNI invalido2', ()=> {
+
+            let dniValido = validaDNI("N");
+            expect(dniValido).toBeFalse();
+
+        })
+
+    })
+
+    describe('Ejercicio 6', () =>{
+        
+        it('palindromo correcto', ()=> {
+
+            let p = palindromo("oso");
+            expect(p).toBeTrue;
+
+        })
+
+        it('palindromo incorrecto', ()=> {
+
+            let p = palindromo("Lidia");
+            expect(p).toBeTrue;
+
+        })
     })
 })
