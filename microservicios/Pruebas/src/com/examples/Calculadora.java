3f662100fa1736703ddfd1763cd65819aad6155d
algarrido <97lidialgm@gmail.com>
2021-10-18 17:09:39 +0200

    eclipse

---
+++ microservicios/Pruebas/src/com/examples/Calculadora.java
@@ -0,0 +1,15 @@
+package com.examples;
+
+public class Calculadora {
+	
+	public double sumar(double a, double b) {
+		return a + b;
+	}
+	public double divide(double a, double b) {
+		return a / b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
+
---
+++ microservicios/Pruebas/src/com/examples/CalculadoraTest.java
@@ -0,0 +1,54 @@
+package com.examples;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+	Calculadora calc;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		//crea una nueva instancia para cada prueba
+		calc = new Calculadora();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSumar() {
+		assertEquals(4, calc.sumar(2, 2));
+		
+	}
+
+	@Test
+	void test_Divide_Double_Double() {
+		assertEquals(0.5, calc.divide(1.0, 2.0));
+		//assertThrows(Exception.class, () -> calc.divide(1.0, 2.0));
+		assertEquals(Double.POSITIVE_INFINITY, calc.divide(1.0, 0));
+
+		
+	}
+
+	@Test
+	@DisplayName("Division entera")
+	void testDivideIntInt() {
+		assertEquals(1, calc.divide(2, 2));
+		
+	}
+
+}
